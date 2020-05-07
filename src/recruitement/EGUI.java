c93ec1464142ee1334591be485b0acc2e9ac9c0e
mazen <mazenatef508@gmail.com>
2020-05-08 01:58:19 +0200

    added Employer GUI

--- src/recruitement/EGUI.java
+++ src/recruitement/EGUI.java
@@ -1,4 +1,46 @@
 package recruitement;
 
-public class EGUI {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class EGUI extends javax.swing.JFrame {
+    private JButton addVacancyButton;
+    private JButton viewMyVacanciesButton;
+    private JButton updtaeProfileButton;
+    private JButton logOutButton;
+    private JPanel empGUI;
+
+    public EGUI() {
+
+        add (empGUI);
+        setTitle("Wuzzafny");
+        setSize(400,500);
+        setLocationRelativeTo(null);
+
+        addVacancyButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        viewMyVacanciesButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        updtaeProfileButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        logOutButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
 }
